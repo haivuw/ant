c06083a261fb9947efdbdd1e2ffbd9d7cf90b026
haivuw <haivuw@gmail.com>
2021-10-12 00:21:09 +0700

    init

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="bg-green-100 p-10">
+      <div className="text-red-500">Hello</div>
+    </main>
+  );
+}
