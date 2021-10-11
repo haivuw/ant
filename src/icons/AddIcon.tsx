4f41cc5e0b9bbe1767f0bcae9e5e39b4d166b24e
haivuw <haivuw@gmail.com>
2021-10-12 02:22:24 +0700

    buttons

---
+++ src/icons/AddIcon.tsx
@@ -0,0 +1,19 @@
+const AddIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      // viewBox="0 0 20 20"
+    >
+      <path
+        className="b"
+        d="M16.667,11.667h-5v5H10v-5H5V10h5V5h1.667v5h5Z"
+        transform="translate(-0.833 -0.833)"
+        fill="#40bf00"
+      />
+    </svg>
+  );
+};
+
+export default AddIcon;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
+import { Button, DatePicker, Switch } from 'antd';
 import Head from 'next/head';
 import Image from 'next/image';
+import AddIcon from 'icons/AddIcon';
 
 export default function Home() {
+  const handleChange = (val) => {
+    console.log(val);
+  };
+
   return (
-    <main className="bg-green-100 p-10">
+    <main className="p-10">
       <div className="text-red-500">Hello</div>
+      <Button className="h-7 p-2 flex items-center rounded-md">
+        <span>
+          <AddIcon />
+        </span>
+        Add check
+      </Button>
+
+      <Button className="h-7 p-2 flex items-center rounded-md">
+        11 checked
+        <Switch size="small" />
+      </Button>
     </main>
   );
 }
